0f5a5b46eb6091d644601ba0e0fb7acec84fc20e
elgynart <elgynart@gmail.com>
2023-02-24 15:10:56 +0200

    created find method

--- index.js
+++ index.js
@@ -186,6 +186,77 @@ class Database {
         return this;
     }
 
+    find(object = {}) {
+        let object_keys = (object)
+            ? Object.keys(object)
+            : [];
+
+        if(object_keys.length > 0 && !Array.isArray(object)) {
+            let data = [];
+            data = this.#temp_arr.reduce((arr, file) => {
+                const file_info = fs.readFileSync(`${this.#path}\\${file}.json`);
+                const json = JSON.parse(file_info);
+
+                let found = true;
+                object_keys.forEach(key => {
+                    if(Object.hasOwn(json, key) && object[key]) {
+                        if(json[key].toString().indexOf(object[key]) === -1) {
+                            found = false;
+                        }
+
+                        if (/(<|>|=)\s?[0-9]/.test(object[key])) {
+                            const conditions = object[key].split(/,\s?/);
+                            
+                            for (let condition of conditions) {
+                                const data_num = condition.match(/[0-9]+/);
+                                const regex_data_number = /^\s?([>|<|=]{1,2})?\s?[0-9]+\s?$/;
+                                const symbol_str = condition.match(regex_data_number);
+
+                                if(symbol_str) {
+                                    const compare = {
+                                        '>': function(num1, num2) {
+                                            return this._(num1, num2) > 0;
+                                        },
+                                        '<': function(num1, num2) {
+                                            return this._(num1, num2) < 0;
+                                        },
+                                        '>=': function(num1, num2) {
+                                            return this._(num1, num2) >= 0;
+                                        },
+                                        '<=': function(num1, num2) {
+                                            return this._(num1, num2) <= 0;
+                                        },
+                                        _(num1, num2) {
+                                            return parseInt(num1) - parseInt(num2);
+                                        }
+                                    }
+
+                                    const num_from_file = json[key];
+                                    const num_from_object = data_num;
+                                    if(Object.hasOwn(compare, symbol_str[1])) {
+                                        found = compare[symbol_str[1]](num_from_file, num_from_object);
+                                    }
+                                }
+
+                                if (!found) break;
+                            }
+                        }
+                    }
+                });
+
+                if(found) {
+                    arr.push(file);
+                }
+
+                return arr;
+            }, []);
+
+            this.#temp_arr = [...data];
+        }
+
+        return this;
+    }
+
     get(offset = null, count = null) {
         let rows = this.#temp_arr;
         let data = [];
